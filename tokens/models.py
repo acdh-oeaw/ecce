f55cd3de008dcef2dd12ebe47a1d4b76fa7511ae
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2016-12-17 17:03:37 +0100

    initial commit

---
+++ tokens/models.py
@@ -0,0 +1,110 @@
+from django.db import models
+from vocabs.models import SkosConcept
+
+
+class Date(models.Model):
+    dates = models.IntegerField(blank=True)
+    decade = models.IntegerField(blank=True)
+    semicentury = models.IntegerField(blank=True)
+    century = models.IntegerField(blank=True)
+    pr_cc_checekd = models.FloatField(blank=True)
+    pr_cc_final_V = models.FloatField(blank=True)
+    pr_cc_both = models.FloatField(blank=True)
+    pr_cc_no = models.IntegerField(blank=True)
+    legacy_id = models.CharField(blank=True, null=True, max_length=100)
+
+
+class Corpus(models.Model):
+    name = models.CharField(blank=True, null=True, max_length=100)
+
+
+class Text(models.Model):
+    text = models.CharField(blank=True, null=True, max_length=100)
+    date = models.CharField(blank=True, null=True, max_length=100)
+    genre = models.ForeignKey(SkosConcept, blank=True, null=True, related_name="skos_genre")
+    corpus = models.ForeignKey(Corpus, blank=True, null=True, related_name="text_corpus")
+    lower = models.IntegerField(blank=True)
+    mean_date = models.ForeignKey(Date, blank=True)
+    size = models.FloatField(blank=True)
+    dialect = models.ForeignKey(SkosConcept, blank=True, null=True, related_name="skos_dialect")
+
+
+class Consonant(models.Model):
+    consonant = models.CharField(blank=True, null=True, max_length=1)
+    art_manner = models.ForeignKey(
+        SkosConcept, blank=True, null=True, related_name="skos_art_manner")
+    art_place = models.ForeignKey(
+        SkosConcept, blank=True, null=True, related_name="skos_art_place")
+    voice = models.ForeignKey(
+        SkosConcept, blank=True, null=True, related_name="skos_voice")
+    airflow = models.ForeignKey(
+        SkosConcept, blank=True, null=True, related_name="skos_airflow")
+    sonority = models.IntegerField(blank=True)
+    transcription = models.CharField(blank=True, null=True, max_length=1)
+    place_ord = models.IntegerField(blank=True)
+
+
+class Cluster(models.Model):
+    first_consonant = models.ForeignKey(
+        Consonant, blank=True, null=True, related_name="consonant_first")
+    second_consonant = models.ForeignKey(
+        Consonant, blank=True, null=True, related_name="consonant_second")
+    third_consonant = models.ForeignKey(
+        Consonant, blank=True, null=True, related_name="consonant_third")
+    fourth_consonant = models.ForeignKey(
+        Consonant, blank=True, null=True, related_name="consonant_fourth")
+    size = models.IntegerField(blank=True)
+    ssp = models.IntegerField(blank=True)
+    cv_structure = models.CharField(blank=True, null=True, max_length=10)
+    preferred_consonant = models.CharField(blank=True, null=True, max_length=10)
+    nad_vc = models.IntegerField(blank=True)
+    nad_c1c2 = models.FloatField(blank=True)
+    nad_c2c3 = models.FloatField(blank=True)
+
+
+class TokenLabel(models.Model):
+    label = models.CharField(blank=True, null=True, max_length=15)
+    description = models.TextField(blank=True, null=True)
+    morphonotacticity = models.ForeignKey(
+        SkosConcept, blank=True, null=True, related_name="skos_morphonotacticity")
+
+
+class SchwaPresent(models.Model):
+    spelling = models.CharField(blank=True, null=True, max_length=15)
+    schwaprese = models.ForeignKey(
+        SkosConcept, blank=True, null=True, related_name="skos_schwaprese")
+    is_no = models.NullBooleanField()
+    is_final = models.NullBooleanField()
+    is_checked = models.NullBooleanField()
+    is_both = models.NullBooleanField()
+
+
+class OnSet(models.Model):
+    rightonset = models.CharField(blank=True, null=True, max_length=15)
+    variable = models.NullBooleanField()
+    pre_change = models.CharField(blank=True, null=True, max_length=1)
+    post_change = models.CharField(blank=True, null=True, max_length=1)
+    onset = models.CharField(blank=True, null=True, max_length=15)
+    offset = models.CharField(blank=True, null=True, max_length=15)
+
+
+class Token(models.Model):
+    legacy_id = models.CharField(blank=True, null=True, max_length=15)
+    spelling = models.CharField(blank=True, null=True, max_length=15)
+    left_context = models.CharField(blank=True, null=True, max_length=150)
+    plain_word = models.CharField(blank=True, null=True, max_length=150)
+    pos = models.ForeignKey(
+        SkosConcept, blank=True, null=True, related_name="skos_pos")
+    label = models.ForeignKey(
+        TokenLabel, blank=True, null=True, related_name="token_label")
+    comments = models.CharField(blank=True, null=True, max_length=150)
+    right_context = models.CharField(blank=True, null=True, max_length=150)
+    text_source = models.ForeignKey(
+        Text, blank=True, null=True, related_name="token_text")
+    cluster = models.ForeignKey(
+        Cluster, blank=True, null=True, related_name="token_cluster")
+    file = models.CharField(blank=True, null=True, max_length=50)
+    medial_suffix = models.CharField(blank=True, null=True, max_length=50)
+    final_suffix = models.CharField(blank=True, null=True, max_length=50)
+    spelling2 = models.ForeignKey(SchwaPresent, blank=True, null=True, related_name="token_spelling2")
+    rightonset = models.ForeignKey(OnSet, blank=True, null=True, related_name="token_onset")
---
+++ webpage/metadata.py
@@ -0,0 +1,11 @@
+# this files contains basic metadata about the project. This data will be used
+# (by default) in the base.html and index.html
+
+PROJECT_METADATA = {
+    'title': 'ECCE',
+    'author': 'Peter Andorfer',
+    'subtitle': 'Evolution of Consonant Clusters in English',
+    'description': 'One of the main purposes of this project is to produce an interactive database of English word-final consonant clusters – phonologically speaking highly marked structures which are nevertheless abundant in English. Our aim is to enrich the scientific landscape of phonology with up to now unexplored diachronic aspects of cluster development, showing how coda clusters emerged and why they are historically speaking stable despite their marked status',
+    'github': 'https://github.com/acdh-oeaw/ecce',
+    'purpose_de': 'Das Ziel des Projekts "ECCE" ist die Publikation von Daten über zu englischen finalen Wort-Konsonanten-Clustern.',
+    'purpose_en': 'The purpose of the project "ECCE" is the publication of English word-final consonant clusters.'}
