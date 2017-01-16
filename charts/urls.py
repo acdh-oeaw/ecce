25c37c9001b6a049a6fe09a7d5a9ea6d01c33f57
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2017-01-16 15:55:18 +0100

    added viz tokens per semicentury https://redmine.acdh.oeaw.ac.at/issues/7671

--- charts/urls.py
+++ charts/urls.py
@@ -3,9 +3,11 @@ from . import views
 
 urlpatterns = [
     url(r'^barcharts/$', views.barcharts_view, name='bar_charts'),
-    url(r'^testjson/$', views.test_json, name='test_json'),
     url(r'^piecharts/$', views.piecharts_view, name='pie_charts'),
+    url(r'^linecharts/$', views.linecharts_view, name='line_charts'),
+    url(r'^testjson/$', views.test_json, name='test_json'),
     url(r'^testjsonpie/$', views.test_json_pie, name='test_json_pie'),
     url(r'^data-token-per-genre/$', views.tokens_per_genre, name='data-token-per-genre'),
     url(r'^data-token-per-genre-static/$', views.tokens_per_genre_static, name='data-token-per-genre-static'),
+    url(r'^data-token-per-date/$', views.tokens_per_date, name='data-token-per-date'),
 ]
--- charts/views.py
+++ charts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.db.models import Count
 from collections import Counter
-from tokens.models import Token
+from tokens.models import Token, Date
 from vocabs.models import SkosConcept, SkosConceptScheme
 
 DATA = {"status": "ok",
@@ -32,6 +32,11 @@ DATA_PIECHART = {
         ["LASK", 9], ["Blau Weiß Linz", 1]
     ]
 }
+
+
+def linecharts_view(request):
+    context = {"test": "test"}
+    return render(request, 'charts/line_charts.html', context)
 
 
 def barcharts_view(request):
@@ -77,4 +82,29 @@ def tokens_per_genre(request):
 
 def tokens_per_genre_static(request):
     data = {"measuredObject": "Tokens", "legendy": "# of Tokens", "title": "Tokens per genres", "payload": [["LETTERS_PRIV", 2260], ["FICTION", 3324], ["EDUC_TREATISE", 2237], ["LETTERS_NON-PRIV", 1335], ["BIBLE", 11060], ["SCIENCE_OTHER", 1708], ["PHILOSOPHY", 3289], ["HISTORY", 24427], ["BIOGRAPHY_OTHER", 1178], ["SCIENCE_MEDICINE", 783], ["RULE", 3888], ["RELIG_TREATISE", 46654], ["HANDBOOK_ASTRO", 913], ["SERMON", 18577], ["PHILOSOPHY/FICTION", 2152], ["BIOGRAPHY_LIFE_OF_SAINT", 3687], ["RELIG_TRREATISE", 1895], ["HANDBOOK_MEDICINE", 1002], ["HOMILY", 13500], ["ROMANCE", 8541], ["TRAVELOGUE", 7425], ["HOMILY_POETRY", 5509], ["HANDBOOK_OTHER", 3152], ["DIARY_PRIV", 2396], ["PROCEEDINGS_TRIAL", 2223], ["DRAMA_COMEDY", 1869], ["BIOGRAPHY_AUTO", 644], ["LAW", 1753]], "items": 177400, "ymin": 0, "subtitle": "Tokens per genre", "legendx": "Genres"}
+    return JsonResponse(data)
+
+
+def tokens_per_date(request):
+    payload = []
+    payload_line = []
+    dates = set([x.semicentury for x in Date.objects.all()])
+    for x in sorted(dates):
+        amount = len(Token.objects.filter(text_source__mean_date__semicentury=x))
+        payload.append(["{}".format(x), amount])
+        payload_line.append(amount)
+
+    data = {
+        "items": len(Token.objects.all()),
+        "title": "Tokens per semicentury",
+        "subtitle": "Tokens per semicentury",
+        "legendx": "Semicentury",
+        "legendy": "# of Tokens",
+        "categories": sorted(dates),
+        "measuredObject": "Tokens",
+        "ymin": 0,
+        "payload": payload,
+        "payload_line": payload_line
+    }
+
     return JsonResponse(data)
