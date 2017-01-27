cb12102d3eb71ae8cf16f206832c8c3884a9bc45
pandorfer <peter.andorfer@oeaw.ac.at>
2017-01-27 15:25:06 +0100

    added wsgi file

--- ecce/wsgi.py
+++ ecce/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ecce project.
+WSGI config for ted project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -7,10 +7,16 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
+
 import os
+import sys
+
+sys.path.append('/var/www/html')
+sys.path.append('/var/www/html/myenv/lib/python3.5/site-packages')
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ecce.settings")
+os.environ["DJANGO_SETTINGS_MODULE"] =  "ecce.settings.server"
 
 application = get_wsgi_application()
+
