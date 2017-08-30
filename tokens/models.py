2c561e0b531edbde62dbbddbd73a9c6c91357d5c
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2017-08-30 09:01:24 +0200

    verbose name and helptext for class Token added

--- tokens/models.py
+++ tokens/models.py
@@ -195,30 +195,82 @@ class OnSet(models.Model):
 
 
 class Token(models.Model):
-    legacy_id = models.CharField(blank=True, null=True, max_length=15)
-    spelling = models.CharField(blank=True, null=True, max_length=15)
-    left_context = models.CharField(blank=True, null=True, max_length=150)
-    plain_word = models.CharField(blank=True, null=True, max_length=150)
+    legacy_id = models.CharField(
+        blank=True, null=True, max_length=15, verbose_name="Identifier",
+        help_text="Canonical identifier of this token"
+    )
+    spelling = models.CharField(
+        blank=True, null=True, max_length=15, verbose_name="search string",
+        help_text="Search string used to find this token"
+    )
+    left_context = models.CharField(
+        blank=True, null=True, max_length=150, verbose_name="Left context",
+        help_text="Left context of word token the cluster is part of"
+    )
+    plain_word = models.CharField(
+        blank=True, null=True, max_length=150, verbose_name="Word form",
+        help_text="Word token the cluster is part of"
+    )
     pos = models.ForeignKey(
-        SkosConcept, blank=True, null=True, related_name="skos_pos")
+        SkosConcept, blank=True, null=True, related_name="skos_pos",
+        verbose_name="Part of speech",
+        help_text="Part of speech of word token the cluster is part of"
+    )
     label = models.ForeignKey(
-        TokenLabel, blank=True, null=True, related_name="token_label")
-    comments = models.CharField(blank=True, null=True, max_length=150)
-    right_context = models.CharField(blank=True, null=True, max_length=150)
+        TokenLabel, blank=True, null=True, related_name="token_label",
+        verbose_name="Morphological status",
+        help_text="Morphological status of cluster (e.g. lexical, derivational, inflectional,...)"
+    )
+    comments = models.CharField(
+        blank=True, null=True, max_length=150, verbose_name="internal comments",
+        help_text="Comments for internal usage"
+    )
+    right_context = models.CharField(
+        blank=True, null=True, max_length=150, verbose_name="Right context",
+        help_text="Right context of word token the cluster is part of"
+    )
     text_source = models.ForeignKey(
-        Text, blank=True, null=True, related_name="token_text")
+        Text, blank=True, null=True, related_name="token_text",
+        verbose_name="Text",
+        help_text="Source text the token is extracted from"
+    )
     cluster = models.ForeignKey(
-        Cluster, blank=True, null=True, related_name="token_cluster")
-    file = models.CharField(blank=True, null=True, max_length=50)
-    medial_suffix = models.CharField(blank=True, null=True, max_length=50)
-    final_suffix = models.CharField(blank=True, null=True, max_length=50)
+        Cluster, blank=True, null=True, related_name="token_cluster",
+        verbose_name="Cluster", help_text="IPA phonological transcription of the cluster"
+    )
+    file = models.CharField(
+        blank=True, null=True, max_length=50, verbose_name="internal analysis file",
+        help_text="Internal file name used for data analysis"
+    )
+    medial_suffix = models.CharField(
+        blank=True, null=True, max_length=50, verbose_name="Medial suffix",
+        help_text="Medial suffix involved in cluster formation")
+    final_suffix = models.CharField(
+        blank=True, null=True, max_length=50, verbose_name="Final suffix",
+        help_text="Final suffix involved in cluster formation"
+    )
     spelling2 = models.ForeignKey(
-        SchwaPresent, blank=True, null=True, related_name="token_spelling2")
-    rightonset = models.ForeignKey(OnSet, blank=True, null=True, related_name="token_onset")
-    updated = models.DateTimeField(auto_now=True)
-    lemma = models.ForeignKey(Lemma, blank=True, null=True)
-    weight = models.FloatField(blank=True, null=True)
-    weight_norm = models.FloatField(blank=True, null=True)
+        SchwaPresent, blank=True, null=True, related_name="token_spelling2",
+        verbose_name="Spelling category", help_text="Graphemic category of the cluster"
+    )
+    rightonset = models.ForeignKey(
+        OnSet, blank=True, null=True, related_name="token_onset",
+        verbose_name="Right phonological context", help_text="Onset of the following word form"
+    )
+    updated = models.DateTimeField(
+        auto_now=True, verbose_name="Date of last update", help_text="Date of last update"
+    )
+    lemma = models.ForeignKey(
+        Lemma, blank=True, null=True, verbose_name="Word lemma",
+        help_text="Lemma of the word form the cluster is part of"
+    )
+    weight = models.FloatField(
+        blank=True, null=True, verbose_name="Weight",
+        help_text="Probabilistically derived weight of the cluster token"
+    )
+    weight_norm = models.FloatField(
+        blank=True, null=True, verbose_name="Per semizentury normalized weight",
+        help_text="Per million tokens normalized probabilistically derived weigth of the cluster token (normalization based on semicentury-wise subcorpora)")
 
     def __str__(self):
         return "{}".format(self.plain_word)
