533d35ff06697e1a3ac3dd2782c174dbcc87a3c3
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2017-09-01 10:48:00 +0200

    little boost of chartselectors template layout

--- charts/urls.py
+++ charts/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^barcharts/$', views.ChartSelector.as_view(), name='chart_selector'),
+    url(r'^chartselector/$', views.ChartSelector.as_view(), name='chart_selector'),
     url(r'^barcharts/(?P<property>[\w\-]+)/$', views.DynChartView.as_view(), name='dynchart'),
 ]
