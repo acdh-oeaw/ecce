d2e2320321c9cba235b39493a75f8ef5e10f4069
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2019-01-07 12:55:29 +0100

    info-props added

--- webpage/metadata.py
+++ webpage/metadata.py
@@ -3,9 +3,19 @@
 
 PROJECT_METADATA = {
     'title': 'ECCE',
-    'author': 'Peter Andorfer',
+    'author': 'Peter Andorfer, Ksenia Zaytseva',
     'subtitle': 'Evolution of Consonant Clusters in English',
     'description': 'One of the main purposes of this project is to produce an interactive database of English word-final consonant clusters – phonologically speaking highly marked structures which are nevertheless abundant in English. Our aim is to enrich the scientific landscape of phonology with up to now unexplored diachronic aspects of cluster development, showing how coda clusters emerged and why they are historically speaking stable despite their marked status',
     'github': 'https://github.com/acdh-oeaw/ecce',
     'purpose_de': 'Das Ziel des Projekts "ECCE" ist die Publikation von Daten über zu englischen finalen Wort-Konsonanten-Clustern.',
-    'purpose_en': 'The purpose of the project "ECCE" is the publication of English word-final consonant clusters.'}
+    'purpose_en': 'The purpose of the project "ECCE" is the publication of English word-final consonant clusters.',
+    'version': '1.0.0',
+    'matomo_id': '49',
+    'matomo_url': '//matomo.acdh.oeaw.ac.at/',
+    'imprint': '/imprint',
+    'social_media': [
+        ('fab fa-twitter fa-2x', 'https://twitter.com/ACDH_OeAW'),
+        ('fab fa-youtube fa-2x', 'https://www.youtube.com/channel/UCgaEMaMbPkULYRI5u6gvG-w'),
+    ],
+    'app_type': 'database',
+}
--- webpage/urls.py
+++ webpage/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.StartView.as_view(), name="start"),
+    url(r'^project-info/$', views.project_info, name='project_info'),
     url(r'^imprint/$', views.ImprintView.as_view(), name='imprint'),
     url(r'^about/$', views.AboutView.as_view(), name='about'),
     url(r'^user-manual/$', views.ManualView.as_view(), name='manual'),
